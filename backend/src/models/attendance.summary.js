4fd1232ee4682777ec0b7d6a34252d8f9f37a3e9
Keshav Arora <keshav11y@gmail.com>
2024-11-06 03:48:17 +0530

    structured folder

--- backend/src/models/attendance.summary.js
+++
@@ -1,9 +0,0 @@
-import {Schema, mongoose} from "mongoose";
-
-const attendanceSchema = new Schema({
-    totalAttendance: {type: Number, default: 0},
-});
-
-const AttendanceSummary = mongoose.model("AttendaceSummary", attendanceSchema);
-
-export {AttendanceSummary};
